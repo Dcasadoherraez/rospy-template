ed7cca305f7f7997362cce3873f5675487a7adb2
Daniel Casado Herraez <decasadoherraez@gmail.com>
2022-12-29 10:12:35 +0100

    Cleanned up

--- src/publisher.py
+++ src/publisher.py
@@ -3,18 +3,22 @@
 import rospy
 from std_msgs.msg import String
 
-def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
-
+class Publisher:
+    def __init__(self, name: str) -> None:
+        rospy.init_node(name, anonymous=True)
+        self.publisher = rospy.Publisher('chatter', String, queue_size=10)
+        self.rate = rospy.Rate(10) # 10hz
+    
+    def pub(self):
+        while not rospy.is_shutdown():
+            hello_str = "hello world %s" % rospy.get_time()
+            rospy.loginfo(hello_str)
+            self.publisher.publish(hello_str)
+            self.rate.sleep()
+    
 if __name__ == '__main__':
+    pub_node = Publisher(name='talker')
     try:
-        talker()
+        pub_node.pub()
     except rospy.ROSInterruptException:
         pass
--- src/subscriber.py
+++ src/subscriber.py
@@ -2,22 +2,18 @@
 import rospy
 from std_msgs.msg import String
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    
-def listener():
+class Subscriber:
+    def __init__(self, name: str) -> None:
+        rospy.init_node(name, anonymous=True)
+        rospy.Subscriber("chatter", String, self.callback)
 
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
+        # spin() simply keeps python from exiting until this node is stopped
+        rospy.spin()
+        
+    def callback(self, data):
+        rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
 
-    rospy.Subscriber("chatter", String, callback)
-
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
 
 if __name__ == '__main__':
-    listener()
+    sub_node = Subscriber(name='listener')
+    
